85c1c14597cb6fdda1826f859a0a9966f4f1871d
mehrabk <mehrab.kor@gmail.com>
2022-08-03 00:51:16 +0430

    implement store provider and store reducer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
+import React, { useReducer } from "react"
 import "../styles/globals.css"
 
+const ACTION_TYPES = {
+  SET_COORDINATE: "SET_COORDINATE",
+  SET_COFFEE_STORES: "SET_COFFEE_STORES"
+}
+
+const storeReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_COORDINATE: {
+      return { ...state, coordinate: action.payload }
+    }
+    case ACTION_TYPES.SET_COFFEE_STORES: {
+      return { ...state, coffeeStores: action.payload }
+    }
+
+    default: {
+      throw new Error("Unhandled Action Type")
+    }
+  }
+}
+
+const StoreContext = React.createContext()
+
+const StoreProvider = ({ children }) => {
+  const initalState = {
+    coordinate: { lat: "", lon: "" },
+    coffeeStores: []
+  }
+
+  const [state, dispatch] = useReducer(storeReducer, initalState)
+  return <StoreContext.Provider value={{ state, dispatch }}>{children}</StoreContext.Provider>
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  )
 }
 
 export default MyApp
